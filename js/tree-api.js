3e552683524401f9725f9f27abd56607e78d37a5
Myroslav <myroslavlutsiv@gmail.com>
2019-08-30 15:48:41 +0300

    [TASK] implement data tree visualization

---
+++ js/tree-api.js
@@ -0,0 +1,1 @@
+!function(){var n={data:[{id:"1",parent:0},{id:"2",parent:"1"},{id:"3",parent:"1"},{id:"4",parent:"3"},{id:"5",parent:"3"},{id:"6",parent:"3"},{id:"7",parent:"4"},{id:"8",parent:"2"}]};window.TreeAPI={getData:function(){return new Promise(function(t){setTimeout(function(){t(n)},1e3*Math.random())})}}}();
---
+++ js/tree.js
@@ -0,0 +1,29 @@
+class TreeBuilder {
+    constructor($container) {
+      this.$container = $container
+    }
+  
+    init (data) {
+      this.render(this.listToTree(data))
+    }
+
+    getTemplateString(dataTree = []) {
+      const itemTemplate = (item, callback) => `<li><span>${item.id}</span>${callback(item.children)}</li>`;
+      const _map = (dataTree) => dataTree.length ? `<ul>${dataTree.map(item => itemTemplate(item, _map))}</ul>` : '';
+      return _map(dataTree);
+    }
+
+    render(dataTree) {
+      let templateString = this.getTemplateString(dataTree).replace(/,/g , '');
+      this.$container.appendChild(document.createRange().createContextualFragment(templateString));
+    }
+
+    listToTree (arr) {
+        const _createNestedArray = (items, id = 1) => {
+          return items.filter(item => item['parent'] == id).map(item => (
+            { ...item, children: _createNestedArray(items, item.id)}
+          ));
+        }
+      return _createNestedArray(arr);
+    }
+}
